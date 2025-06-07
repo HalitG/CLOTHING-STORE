1cc0ef2e22e59fbd483db056f21dcbbb7a897db1
Halit Gashi <gashihalit1@gmail.com>
2025-06-07 23:00:09 +0200

    Added Upload for uploading images

---
+++ backend/middleware/upload.js
@@ -0,0 +1,34 @@
+const multer = require("multer");
+const path = require("path");
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "upload/products/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Data.now() + "-" + Math.round(Math.random) * 1e9;
+    cd(
+      null,
+      file.filename + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, type);
+  } else {
+    cb(new Error("Not an image! Please upload an image"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB limit
+  },
+});
+
+module.exports = upload;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,8 @@ const bcrypt = require("bcrypt.js");
 const jwt = require("jsonwebtoken");
 const db = require(`../config/database`);
 
-router.post("register", async (req, res) => {
+// Register new user
+router.post("/register", async (req, res) => {
   const { email, password, first_name, last_name } = req.body;
 
   try {
@@ -53,6 +54,56 @@ router.post("register", async (req, res) => {
   } catch (error) {
     console.log("Error: ", error);
     res.status(500).json({ message: "Error registering user" });
+  }
+});
+
+// Login user
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Validate input
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide email and password" });
+    }
+
+    // check if user exists
+    const [users] = await db.query(
+      "SELECT id, email, password, first_name, last_name, role from users WHERE email=?",
+      [email]
+    );
+
+    if (user.length == 0) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const user = users[0];
+
+    // Verify password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_TOKEN,
+      { expiresIn: "24h" }
+    );
+
+    // Remove password from response
+    delete user.password;
+
+    res.json({
+      token,
+      user,
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ message: "Error logging in" });
   }
 });
 
