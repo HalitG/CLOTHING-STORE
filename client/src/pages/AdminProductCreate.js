46f18490fe0a77a43b53b86568bf54214c807285
Halit Gashi <gashihalit1@gmail.com>
2025-06-16 03:07:25 +0200

    Initial setup for AdminProductCreate page

--- client/src/pages/AdminProductCreate.js
+++ client/src/pages/AdminProductCreate.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, use } from "react";
+import { useNavigate } from "react-router-dom";
 import ProductForm from "../components/ProductForm";
 
 const AdminProductCreate = () => {
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category_id: "",
+    stock: "",
+  });
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/categories", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const data = response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error(`Error fetching categories:`, error);
+    }
+  };
+
   return <ProductForm />;
 };
 
